a69d6a82a4910166fbdb0594a6d5c0c0fe688e50
kartikeymalvi <kartikeymalviya02@gmail.com>
2026-06-17 15:15:16 +0530

    Added Live Backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -50,7 +50,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://shreemaa-backend.onrender.com/api/",
 });
 
 // REQUEST INTERCEPTOR
